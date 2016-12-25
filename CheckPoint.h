985cf8837c93d2527d391bb391e74a7820be5685
amitrdt <amitrdt@gmail.com>
2016-12-25 18:44:41 +0200

    noname

--- CheckPoint.h
+++ CheckPoint.h
@@ -22,7 +22,7 @@ private:
     bool forthNeighbor;
     //public member section.
 public:
-    void noname();
+
     void setFirstNeighbor(bool);
     void setSecondNeighbor(bool);
     void setThirdNeighbor(bool);
